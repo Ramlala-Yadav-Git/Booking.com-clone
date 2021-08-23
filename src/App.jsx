274d9686b5f3716b7882007f06ca3da97aaa3dd0
Ramlala Yadav <easyexam01@gmail.com>
2021-08-23 22:51:56 +0530

    created new repo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+// import { BrowserRouter } from "react-router-dom"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi</h1>
+    </div>
+  );
+}
+
+export default App;
