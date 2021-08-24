094e78d486ed2493acd1c9ff6110bdff6631e676
Rakesh-pilly <rakesh.mudhiraj882@gmail.com>
2021-08-24 19:48:28 +0530

    created box

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { Navbar } from "./Components/Navbar/Navbar";
+import { NextTrip } from "./Components/NextTripDiv/NextTrip";
 
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+
+      <NextTrip/>
     </div>
   );
 }
--- src/Components/NextTripDiv/NextTrip.jsx
+++ src/Components/NextTripDiv/NextTrip.jsx
@@ -1,3 +1,19 @@
+import { ImageContainer } from "./ImageContainer"
 
 
+export const NextTrip = () => {
 
+
+return(
+    <div>
+        <h1>Get inspiration for your next trip</h1>
+
+        <div>
+            <ImageContainer wide = "320px" img = "https://cf.bstatic.com/xdata/images/xphoto/540x405/122925057.webp?k=09f14b17546723a74f3c71cc9d5e5040e60c1594234b5a06cc3ce376ad299da5&o=">
+    <h1>check what </h1>
+            </ImageContainer>
+        </div>
+    </div>
+)
+
+}
