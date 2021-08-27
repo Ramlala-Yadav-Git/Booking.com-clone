b46d081be2ba8a029f943e5fcbcf1861fa17a386
= <=>
2021-08-27 18:22:51 +0530

    login-signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { Navbar } from "./Components/Navbar/Navbar"
-import { SearchDeals } from "./Components/SearchDeals/SearchDeals"
-import { SearchRequest } from "./Components/SearchPage/SearchRequest";
+// import { Navbar } from "./Components/Navbar/Navbar"
+// import { SearchDeals } from "./Components/SearchDeals/SearchDeals"
+// import { SearchRequest } from "./Components/SearchPage/SearchRequest";
+import Login from './Components/Login/Login'
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      {/* <Navbar />
       <SearchDeals />
-      <SearchRequest />
+      <SearchRequest /> */}
+      <Login/>
     </div>
   );
 }
